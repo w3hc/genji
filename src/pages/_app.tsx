dca0b6b1bbc6df7f22f3bd12a057131b57aec694
Julien Béranger <julien@strat.cc>
2024-04-09 13:48:42 +0200

    Add Chakra UI (#6)
    * add chakra
    
    * add page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import { useEffect, useState } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [ready, setReady] = useState(false)
@@ -12,12 +13,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {ready ? (
-        // <ChakraProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      ) : // </ChakraProvider>
-      null}
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      ) : null}
     </>
   )
 }
