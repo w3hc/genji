9bfa8babe72b7a286adaca5aea556d58e1e82c0d
julienbrg <julien@strat.cc>
2024-04-09 15:31:09 +0200

    structure

--- src/pages/new/index.tsx
+++ src/pages/new/index.tsx
@@ -1,10 +1,8 @@
 import { Text, Button, useToast } from '@chakra-ui/react'
-import { Header } from '../../components/layout/Header'
 
 export default function New() {
   return (
     <>
-      <Header />
       <main>
         <Text>A brand new page! 😋</Text>
       </main>
