958e05b1afaf31f4627249182100e7df3f560293
julienbrg <julien@strat.cc>
2024-04-09 13:56:16 +0200

    add head

---
+++ src/components/layout/Head.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { default as NextHead } from 'next/head'
+import { SITE_DESCRIPTION, SITE_NAME } from '../../utils/config'
+
+interface Props {
+  title?: string
+  description?: string
+}
+
+export function Head(props: Props) {
+  return (
+    <NextHead>
+      <title>{props.title ?? SITE_NAME} </title>
+      <meta property="og:url" content="https://genji.netlify.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={props.title ?? SITE_NAME} />
+      <meta name="twitter:card" content={SITE_DESCRIPTION} />
+      <meta property="og:description" content={props.description ?? SITE_DESCRIPTION} />
+      <meta property="og:image" content={'./public/huangshan.jpeg'} />
+      <meta name="description" content={props.description ?? SITE_DESCRIPTION} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </NextHead>
+  )
+}
