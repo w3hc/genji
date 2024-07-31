1e63f1d22517247594567433ac24810b5a744695
Julien Béranger <julien@strat.cc>
2024-07-31 17:42:18 +0200

    Raise faucet amount (#24)
    * add react-icons
    
    * add gh logo
    
    * raise faucet amount

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Flex, useColorModeValue, Spacer, Heading } from '@chakra-ui/react'
+import { Flex, useColorModeValue, Spacer, Heading, Box, Link, Icon } from '@chakra-ui/react'
 import { LinkComponent } from './LinkComponent'
 import { ThemeSwitcher } from './ThemeSwitcher'
 import { SITE_NAME } from '../../utils/config'
+import { FaGithub } from 'react-icons/fa'
 
 interface Props {
   className?: string
@@ -23,7 +24,14 @@ export function Header(props: Props) {
 
       <Flex alignItems="center" gap={4}>
         <w3m-button />
-        <ThemeSwitcher />
+        <Flex alignItems="center">
+          <ThemeSwitcher />
+          <Box mt={2} ml={4}>
+            <Link href="https://github.com/w3hc/genji" isExternal>
+              <Icon as={FaGithub} boxSize={5} _hover={{ color: 'blue.500' }} />
+            </Link>
+          </Box>
+        </Flex>
       </Flex>
     </Flex>
   )
