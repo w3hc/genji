cc52c8d92a8e3842672e05e5414d20fe317188fd
julienbrg <julien@strat.cc>
2024-09-30 10:00:42 +0200

    add w3m-button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, useColorModeValue, Spacer, Heading, Box, Link, Icon, Button } from '@chakra-ui/react'
+import { Flex, useColorModeValue, Spacer, Heading, Box, Link, Icon } from '@chakra-ui/react'
 import { LinkComponent } from './LinkComponent'
 import { ThemeSwitcher } from './ThemeSwitcher'
 import { SITE_NAME } from '../../utils/config'
@@ -13,17 +13,6 @@ interface Props {
 
 export function Header(props: Props) {
   const className = props.className ?? ''
-  const { open } = useWeb3Modal()
-  const { isConnected } = useWeb3ModalAccount()
-  const { disconnect } = useDisconnect()
-
-  const handleAuth = async () => {
-    if (isConnected) {
-      await disconnect()
-    } else {
-      await open()
-    }
-  }
 
   return (
     <Flex as="header" className={className} bg={useColorModeValue('gray.100', 'gray.900')} px={4} py={5} mb={8} alignItems="center">
@@ -36,9 +25,7 @@ export function Header(props: Props) {
       <Spacer />
 
       <Flex alignItems="center" gap={4}>
-        <Button onClick={handleAuth} colorScheme="blue" size="sm">
-          {isConnected ? 'Logout' : 'Login'}
-        </Button>
+        <w3m-button />
         <Flex alignItems="center">
           <ThemeSwitcher />
           <Box mt={2} ml={4}>
