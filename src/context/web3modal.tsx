12e9f20a7364e9e25a6ba4e38b46311cedc37ee0
julienbrg <julien@strat.cc>
2024-09-30 10:00:50 +0200

    add farcaster

--- src/context/web3modal.tsx
+++ src/context/web3modal.tsx
@@ -33,7 +33,7 @@ const ethersConfig = defaultConfig({
   auth: {
     email: true,
     // socials: ['google', 'x', 'github', 'discord', 'apple'],
-    socials: ['google'],
+    socials: ['google', 'farcaster'],
     showWallets: true,
     walletFeatures: true,
   },
