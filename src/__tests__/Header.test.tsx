3583e23b76d80ec69c829ef40f62d3bd2dc6bc1e
julienbrg <julien@strat.cc>
2024-11-02 19:35:28 +0100

    fix tests

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../utils/test-utils'
 import { Header } from '@/components/Header'
 import '@testing-library/jest-dom'
 
 describe('Header', () => {
   it('exists in the document', () => {
     render(<Header />)
-    // Basic test to check if header renders at all
     expect(document.querySelector('header')).toBeInTheDocument()
   })
 })
